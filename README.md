71424d16f08ba162e3162007f0dee9c0e470b5c5
Leandra Tejedor <lmtejedor@gmail.com>
2014-09-29 00:17:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CenturyLink Field Safety Tips
+
+
+
+## Description
+
+Have you wondered when it's safest to visit CenturyLink field? Well, now through the use of [Socrata's Open Data](http://dev.socrata.com/), you can find out.
+
+Visit [the live demo](http://ltejedor.github.io/crime-tracker) to find out more.
---
+++ js/main.js
@@ -0,0 +1,318 @@
+$( document ).ready(function() {
+	//on page load, graph by time is showing
+	getData(crimesByTime);
+
+	$('.js-show-month').click(function(){
+		$('.js-show-time').removeClass('is-active');
+		$(this).addClass('is-active');
+		getData(crimesByWeekday);
+	});
+
+	$('.js-show-time').click(function(){
+		$('.js-show-month').removeClass('is-active');
+		$(this).addClass('is-active');
+		getData(crimesByTime);
+	});
+
+});
+
+var getData = function(graphType){
+	$.ajax({
+		type: "GET",
+		cache: false,
+		url: "https://data.seattle.gov/resource/3k2p-39jp.json?$where=within_circle(incident_location, 47.595146, -122.331601, 1609.34)",
+		})
+	.done(function(data){
+		//send data from api to either the weekday or time bar graphs
+		graphType(data);
+	});
+}
+
+var crimesByWeekday = function(data){
+	var dayOfWeek = {
+		'Mon': 0,
+		'Tue': 0,
+		'Wed': 0,
+		'Thu': 0,
+		'Fri': 0,
+		'Sat': 0,
+		'Sun': 0,
+	};
+	for(var i=0; i<data.length; i++){
+		var thisDate = (data[i].event_clearance_date);
+		var thisDay = thisDate.substring(0, 10);
+		var dayName = String(Date.parse(thisDay)).substring(0, 3);
+
+		dayOfWeek[dayName]++;
+	}
+
+	getValues(dayOfWeek, 'day');
+
+	var chart = AmCharts.makeChart("chartdiv", {
+	  "type": "serial",
+		"theme": "dark",
+	    "dataProvider": [
+	{
+	        "day": "Monday",
+	        "crime": dayOfWeek['Mon']
+	    },
+	{
+	        "day": "Tuesday",
+	        "crime": dayOfWeek['Tue']
+	    }, {
+	        "day": "Wednesday",
+	        "crime": dayOfWeek['Wed']
+	    }, {
+	        "day": "Thursday",
+	        "crime": dayOfWeek['Thu']
+	    }, {
+	        "day": "Friday",
+	        "crime": dayOfWeek['Fri']
+	    }, {
+	        "day": "Saturday",
+	        "crime": dayOfWeek['Sat']
+	    }, {
+	        "day": "Sunday",
+	        "crime": dayOfWeek['Sun']
+	    }],
+	    "valueAxes": [{
+      "gridColor":"#FFFFFF",
+			"gridAlpha": 0.2,
+			"dashLength": 0
+	    }],
+	    "gridAboveGraphs": true,
+	    "startDuration": 1,
+	    "graphs": [{
+	        "balloonText": "[[category]]: <b>[[value]]</b>",
+	        "fillAlphas": 0.8,
+	        "lineAlpha": 0.2,
+	        "type": "column",
+	        "valueField": "crime"
+	    }],
+	    "chartCursor": {
+	        "categoryBalloonEnabled": false,
+	        "cursorAlpha": 0,
+	        "zoomable": false
+	    },
+	    "categoryField": "day",
+	    "categoryAxis": {
+	        "gridPosition": "start",
+	        "gridAlpha": 0,
+	         "tickPosition":"start",
+	         "tickLength":20
+	    },
+		"exportConfig":{
+		  "menuTop": 0,
+		  "menuItems": [{
+	      "icon": '/lib/3/images/export.png',
+	      "format": 'png'
+	      }]
+		}
+	});
+}
+
+
+var crimesByTime = function(data){
+	var timeOfDay = {
+		'00': 0,
+		'01': 0,
+		'02': 0,
+		'03': 0,
+		'04': 0,
+		'05': 0,
+		'06': 0,
+		'07': 0,
+		'08': 0,
+		'09': 0,
+		'10': 0,
+		'11': 0,
+		'12': 0,
+		'13': 0,
+		'14': 0,
+		'15': 0,
+		'16': 0,
+		'17': 0,
+		'18': 0,
+		'19': 0,
+		'20': 0,
+		'21': 0,
+		'22': 0,
+		'23': 0
+	};
+	for(var i=0; i<data.length; i++){
+		var thisDate = (data[i].event_clearance_date);
+		var thisTime = thisDate.substring(11, 13);
+
+		timeOfDay[thisTime]++;
+
+	}
+
+	getValues(timeOfDay, 'time');
+
+	var chart = AmCharts.makeChart("chartdiv", {
+	  "type": "serial",
+		"theme": "dark",
+	    "dataProvider": [
+	{
+	        "time": "12am",
+	        "crime": timeOfDay['00']
+	    },
+	{
+	        "time": "1am",
+	        "crime": timeOfDay['01']
+	    }, {
+	        "time": "2am",
+	        "crime": timeOfDay['02']
+	    }, {
+	        "time": "3am",
+	        "crime": timeOfDay['03']
+	    }, {
+	        "time": "4am",
+	        "crime": timeOfDay['04']
+	    }, {
+	        "time": "5am",
+	        "crime": timeOfDay['05']
+	    }, {
+	        "time": "6am",
+	        "crime": timeOfDay['06']
+	    }, {
+	        "time": "7am",
+	        "crime": timeOfDay['07']
+	    }, {
+	        "time": "8am",
+	        "crime": timeOfDay['08']
+	    },
+	    {
+	        "time": "9am",
+	        "crime": timeOfDay['09']
+	    },
+	    {
+	        "time": "10am",
+	        "crime": timeOfDay['10']
+	    },
+	    {
+	        "time": "11am",
+	        "crime": timeOfDay['11']
+	    },
+	    {
+	        "time": "12pm",
+	        "crime": timeOfDay['12']
+	    },
+	    {
+	        "time": "1pm",
+	        "crime": timeOfDay['13']
+	    },
+	    {
+	        "time": "2pm",
+	        "crime": timeOfDay['14']
+	    },
+	    {
+	        "time": "3pm",
+	        "crime": timeOfDay['15']
+	    },
+	    {
+	        "time": "4pm",
+	        "crime": timeOfDay['16']
+	    },
+	    {
+	        "time": "5pm",
+	        "crime": timeOfDay['17']
+	    },
+	    {
+	        "time": "6pm",
+	        "crime": timeOfDay['18']
+	    },
+	    {
+	        "time": "7pm",
+	        "crime": timeOfDay['19']
+	    },
+	    {
+	        "time": "8pm",
+	        "crime": timeOfDay['20']
+	    },
+	    {
+	        "time": "9pm",
+	        "crime": timeOfDay['21']
+	    },
+	    {
+	        "time": "10pm",
+	        "crime": timeOfDay['22']
+	    },
+	    {
+	        "time": "11pm",
+	        "crime": timeOfDay['23']
+	    }],
+	    "valueAxes": [{
+      "gridColor":"#FFFFFF",
+			"gridAlpha": 0.2,
+			"dashLength": 0
+	    }],
+	    "gridAboveGraphs": true,
+	    "startDuration": 1,
+	    "graphs": [{
+	        "balloonText": "[[category]]: <b>[[value]]</b>",
+	        "fillAlphas": 0.8,
+	        "lineAlpha": 0.2,
+	        "type": "column",
+	        "valueField": "crime"
+	    }],
+	    "chartCursor": {
+	        "categoryBalloonEnabled": false,
+	        "cursorAlpha": 0,
+	        "zoomable": false
+	    },
+	    "categoryField": "time",
+	    "categoryAxis": {
+	        "gridPosition": "start",
+	        "gridAlpha": 0,
+	         "tickPosition":"start",
+	         "tickLength":20
+	    },
+		"exportConfig":{
+		  "menuTop": 0,
+		  "menuItems": [{
+	      "icon": '/lib/3/images/export.png',
+	      "format": 'png'
+	      }]
+		}
+	});
+}
+
+var getValues = function(thisObj, objType){
+	var max = 0;
+	var min = 100;
+	for (var prop in thisObj) {
+		if(thisObj[prop] > max){
+			var max = thisObj[prop];
+			var maxProp = prop;
+		}
+	}
+	for (var prop in thisObj) {
+		if(thisObj[prop] < min){
+			var min = thisObj[prop];
+			var minProp = prop;
+		}
+	}
+	if(objType === 'time'){
+		var valueMost = convertTime(maxProp);
+		var valueMin = convertTime(minProp);
+	} else {
+		var valueMost = maxProp;
+		var valueMin = minProp;
+	}
+
+	$('.js-type').text(objType);
+	$('.js-d-most').text(valueMost);
+	$('.js-d-least').text(valueMin);
+
+}
+
+var convertTime = function(tProperty){
+	if(tProperty === 0){
+		return "12am";
+	} else if(tProperty > 12){
+		return tProperty - 12 + "pm";
+	}else{
+		return tProperty + "am";
+	}
+}
